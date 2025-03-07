4949b6e3df6d16eeafea5563d8548273e5860794
Zimoo343 <davidruiz7.dev@gmail.com>
2025-03-06 21:14:22 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,15 +8,12 @@ const nextConfig: NextConfig = {
         hostname: "cdn.sanity.io",
       }
     ]
-  }
-};
-
-module.exports = {
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-}
+  }
+};
 
 export default nextConfig;
