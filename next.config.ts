1b05822f585ae80ef0662aa6d0fed2c0018ed802
Zimoo343 <davidruiz7.dev@gmail.com>
2025-03-06 21:34:49 -0600

    EsLint Rules Try

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,6 @@ const nextConfig: NextConfig = {
         hostname: "cdn.sanity.io",
       }
     ]
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
   }
 };
 
